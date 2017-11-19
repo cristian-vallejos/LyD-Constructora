6f311c1a65f3e97f3b1212eb0109a12176fccefa
Cristian Vallejos <cristian.vallejos.14@sansano.usm.cl>
2017-11-19 15:48:18 -0300

    Mejoras al panel de Solicitud de Pŕestamo en cuanto a modificación de valores monetarios

--- app/assets/javascripts/formato_moneda.js
+++ app/assets/javascripts/formato_moneda.js
@@ -1,4 +1,18 @@
+
+//  Monto Solicitado
 document.getElementById("loanformulary_monto_solicitado").onchange =function (){
+
+    if (document.getElementById("loanformulary_monto_solicitado").value != "" && isNaN(document.getElementById("loanformulary_monto_solicitado").value)) {
+      var array_solicitado = document.getElementById("loanformulary_monto_solicitado").value.split(' ')[1].split('.');
+      var tamano_solicitado = array_solicitado.length; var i = 0;
+      var monto_solicitado = 0;
+      while (tamano_solicitado > 0) {
+        monto_solicitado += array_solicitado[i]*Math.pow(1000, tamano_solicitado-1);
+        tamano_solicitado -= 1; i += 1;
+      }
+      document.getElementById("loanformulary_monto_solicitado").value = monto_solicitado;
+    }
+
     this.value = parseFloat(this.value.replace(/,/g, ""))
                     .toFixed(0)
                     .toString()
@@ -7,7 +21,20 @@ document.getElementById("loanformulary_monto_solicitado").onchange =function (){
     document.getElementById("loanformulary_monto_solicitado").value = "$ "+this.value.replace(/,/g, "");
 }
 
+//  Monto Aprobado
 document.getElementById("loanformulary_monto_aprobado").onchange =function (){
+
+    if (document.getElementById("loanformulary_monto_aprobado").value != "" && isNaN(document.getElementById("loanformulary_monto_aprobado").value)) {
+      var array_aprobado = document.getElementById("loanformulary_monto_aprobado").value.split(' ')[1].split('.');
+      var tamano_aprobado = array_aprobado.length; var i = 0;
+      var monto_aprobado = 0;
+      while (tamano_aprobado > 0) {
+        monto_aprobado += array_aprobado[i]*Math.pow(1000, tamano_aprobado-1);
+        tamano_aprobado -= 1; i += 1;
+      }
+      document.getElementById("loanformulary_monto_aprobado").value = monto_aprobado;
+    }
+
     this.value = parseFloat(this.value.replace(/,/g, ""))
                     .toFixed(0)
                     .toString()
@@ -34,6 +61,7 @@ document.getElementById("loanformulary_monto_aprobado").onchange =function (){
     }
 }
 
+//  Cantidad de Cuotas
 document.getElementById("loanformulary_numero_cuotas").onblur = function (){
 
     if (document.getElementById("loanformulary_monto_aprobado").value != "") {
@@ -56,7 +84,20 @@ document.getElementById("loanformulary_numero_cuotas").onblur = function (){
     }
 }
 
+//  Renta Líquida
 document.getElementById("loanformulary_renta_liquida").onchange =function (){
+
+    if (document.getElementById("loanformulary_renta_liquida").value != "" && isNaN(document.getElementById("loanformulary_renta_liquida").value)) {
+      var array_renta_liquida = document.getElementById("loanformulary_renta_liquida").value.split(' ')[1].split('.');
+      var tamano_renta_liquida = array_renta_liquida.length; var i = 0;
+      var renta_liquida = 0;
+      while (tamano_renta_liquida > 0) {
+        renta_liquida += array_renta_liquida[i]*Math.pow(1000, tamano_renta_liquida-1);
+        tamano_renta_liquida -= 1; i += 1;
+      }
+      document.getElementById("loanformulary_renta_liquida").value = renta_liquida;
+    }
+
     this.value = parseFloat(this.value.replace(/,/g, ""))
                     .toFixed(0)
                     .toString()
@@ -65,7 +106,20 @@ document.getElementById("loanformulary_renta_liquida").onchange =function (){
     document.getElementById("loanformulary_renta_liquida").value = "$ "+this.value.replace(/,/g, "");
 }
 
+//  Fondo Finiquito
 document.getElementById("loanformulary_fondo_finiquito").onchange =function (){
+
+    if (document.getElementById("loanformulary_fondo_finiquito").value != "" && isNaN(document.getElementById("loanformulary_fondo_finiquito").value)) {
+      var array_finiquito = document.getElementById("loanformulary_fondo_finiquito").value.split(' ')[1].split('.');
+      var tamano_finiquito = array_finiquito.length; var i = 0;
+      var monto_finiquito = 0;
+      while (tamano_finiquito > 0) {
+        monto_finiquito += array_finiquito[i]*Math.pow(1000, tamano_finiquito-1);
+        tamano_finiquito -= 1; i += 1;
+      }
+      document.getElementById("loanformulary_fondo_finiquito").value = monto_finiquito;
+    }
+
     this.value = parseFloat(this.value.replace(/,/g, ""))
                     .toFixed(0)
                     .toString()
@@ -74,7 +128,20 @@ document.getElementById("loanformulary_fondo_finiquito").onchange =function (){
     document.getElementById("loanformulary_fondo_finiquito").value = "$ "+this.value.replace(/,/g, "");
 }
 
+//  Descuentos En Curso
 document.getElementById("loanformulary_descuentos_en_curso").onchange =function (){
+
+    if (document.getElementById("loanformulary_descuentos_en_curso").value != "" && isNaN(document.getElementById("loanformulary_descuentos_en_curso").value)) {
+      var array_descuentos_en_curso = document.getElementById("loanformulary_descuentos_en_curso").value.split(' ')[1].split('.');
+      var tamano_descuentos_en_curso = array_descuentos_en_curso.length; var i = 0;
+      var descuentos_en_curso = 0;
+      while (tamano_descuentos_en_curso > 0) {
+        descuentos_en_curso += array_descuentos_en_curso[i]*Math.pow(1000, tamano_descuentos_en_curso-1);
+        tamano_descuentos_en_curso -= 1; i += 1;
+      }
+      document.getElementById("loanformulary_descuentos_en_curso").value = descuentos_en_curso;
+    }
+
     this.value = parseFloat(this.value.replace(/,/g, ""))
                     .toFixed(0)
                     .toString()
@@ -83,70 +150,67 @@ document.getElementById("loanformulary_descuentos_en_curso").onchange =function 
     document.getElementById("loanformulary_descuentos_en_curso").value = "$ "+this.value.replace(/,/g, "");
 }
 
+/*************************************
+**  Función de reversión monetaria  **
+*************************************/
+
 function revertirMoneda() {
-  var array_solicitado = document.getElementById("loanformulary_monto_solicitado").value.split(' ')[1].split('.');
-  var tamano_solicitado = array_solicitado.length; var i = 0;
-  var monto_solicitado = 0;
-  while (tamano_solicitado > 0) {
-    monto_solicitado += array_solicitado[i]*Math.pow(1000, tamano_solicitado-1);
-    tamano_solicitado -= 1; i += 1;
-  }
-  document.getElementById("loanformulary_monto_solicitado").value = monto_solicitado;
+    var array_solicitado = document.getElementById("loanformulary_monto_solicitado").value.split(' ')[1].split('.');
+    var tamano_solicitado = array_solicitado.length; var i = 0;
+    var monto_solicitado = 0;
+    while (tamano_solicitado > 0) {
+      monto_solicitado += array_solicitado[i]*Math.pow(1000, tamano_solicitado-1);
+      tamano_solicitado -= 1; i += 1;
+    }
+    document.getElementById("loanformulary_monto_solicitado").value = monto_solicitado;
+    
 
-  console.log("Solicitado: "+array_solicitado);
+    var array_aprobado = document.getElementById("loanformulary_monto_aprobado").value.split(' ')[1].split('.');
+    var tamano_aprobado = array_aprobado.length; var i = 0;
+    var monto_aprobado = 0;
+    while (tamano_aprobado > 0) {
+      monto_aprobado += array_aprobado[i]*Math.pow(1000, tamano_aprobado-1);
+      tamano_aprobado -= 1; i += 1;
+    }
+    document.getElementById("loanformulary_monto_aprobado").value = monto_aprobado;
 
-  var array_aprobado = document.getElementById("loanformulary_monto_aprobado").value.split(' ')[1].split('.');
-  var tamano_aprobado = array_aprobado.length; var i = 0;
-  var monto_aprobado = 0;
-  while (tamano_aprobado > 0) {
-    monto_aprobado += array_aprobado[i]*Math.pow(1000, tamano_aprobado-1);
-    tamano_aprobado -= 1; i += 1;
-  }
-  document.getElementById("loanformulary_monto_aprobado").value = monto_aprobado;
 
-  console.log("Aprobado: "+array_aprobado);
+    var array_cuota = document.getElementById("loanformulary_monto_cuota").value.split(' ')[1].split('.');
+    var tamano_cuota = array_cuota.length; var i = 0;
+    var monto_cuota = 0;
+    while (tamano_cuota > 0) {
+      monto_cuota += array_cuota[i]*Math.pow(1000, tamano_cuota-1);
+      tamano_cuota -= 1; i += 1;
+    }
+    document.getElementById("loanformulary_monto_cuota").value = monto_cuota;
 
-  var array_cuota = document.getElementById("loanformulary_monto_cuota").value.split(' ')[1].split('.');
-  var tamano_cuota = array_cuota.length; var i = 0;
-  var monto_cuota = 0;
-  while (tamano_cuota > 0) {
-    monto_cuota += array_cuota[i]*Math.pow(1000, tamano_cuota-1);
-    tamano_cuota -= 1; i += 1;
-  }
-  document.getElementById("loanformulary_monto_cuota").value = monto_cuota;
 
-  console.log("Cuota: "+array_cuota);
+    var array_renta_liquida = document.getElementById("loanformulary_renta_liquida").value.split(' ')[1].split('.');
+    var tamano_renta_liquida = array_renta_liquida.length; var i = 0;
+    var renta_liquida = 0;
+    while (tamano_renta_liquida > 0) {
+      renta_liquida += array_renta_liquida[i]*Math.pow(1000, tamano_renta_liquida-1);
+      tamano_renta_liquida -= 1; i += 1;
+    }
+    document.getElementById("loanformulary_renta_liquida").value = renta_liquida;
 
-  var array_renta_liquida = document.getElementById("loanformulary_renta_liquida").value.split(' ')[1].split('.');
-  var tamano_renta_liquida = array_renta_liquida.length; var i = 0;
-  var renta_liquida = 0;
-  while (tamano_renta_liquida > 0) {
-    renta_liquida += array_renta_liquida[i]*Math.pow(1000, tamano_renta_liquida-1);
-    tamano_renta_liquida -= 1; i += 1;
-  }
-  document.getElementById("loanformulary_renta_liquida").value = renta_liquida;
 
-  console.log("Renta líquida: "+array_renta_liquida);
+    var array_finiquito = document.getElementById("loanformulary_fondo_finiquito").value.split(' ')[1].split('.');
+    var tamano_finiquito = array_finiquito.length; var i = 0;
+    var monto_finiquito = 0;
+    while (tamano_finiquito > 0) {
+      monto_finiquito += array_finiquito[i]*Math.pow(1000, tamano_finiquito-1);
+      tamano_finiquito -= 1; i += 1;
+    }
+    document.getElementById("loanformulary_fondo_finiquito").value = monto_finiquito;
 
-  var array_finiquito = document.getElementById("loanformulary_fondo_finiquito").value.split(' ')[1].split('.');
-  var tamano_finiquito = array_finiquito.length; var i = 0;
-  var monto_finiquito = 0;
-  while (tamano_finiquito > 0) {
-    monto_finiquito += array_finiquito[i]*Math.pow(1000, tamano_finiquito-1);
-    tamano_finiquito -= 1; i += 1;
-  }
-  document.getElementById("loanformulary_fondo_finiquito").value = monto_finiquito;
 
-  console.log("Finiquito: "+array_finiquito);
-
-  var array_descuentos_en_curso = document.getElementById("loanformulary_descuentos_en_curso").value.split(' ')[1].split('.');
-  var tamano_descuentos_en_curso = array_descuentos_en_curso.length; var i = 0;
-  var descuentos_en_curso = 0;
-  while (tamano_descuentos_en_curso > 0) {
-    descuentos_en_curso += array_descuentos_en_curso[i]*Math.pow(1000, tamano_descuentos_en_curso-1);
-    tamano_descuentos_en_curso -= 1; i += 1;
-  }
-  document.getElementById("loanformulary_descuentos_en_curso").value = descuentos_en_curso;
-
-  console.log("Descuentos: "+array_descuentos_en_curso);
+    var array_descuentos_en_curso = document.getElementById("loanformulary_descuentos_en_curso").value.split(' ')[1].split('.');
+    var tamano_descuentos_en_curso = array_descuentos_en_curso.length; var i = 0;
+    var descuentos_en_curso = 0;
+    while (tamano_descuentos_en_curso > 0) {
+      descuentos_en_curso += array_descuentos_en_curso[i]*Math.pow(1000, tamano_descuentos_en_curso-1);
+      tamano_descuentos_en_curso -= 1; i += 1;
+    }
+    document.getElementById("loanformulary_descuentos_en_curso").value = descuentos_en_curso;
 }
