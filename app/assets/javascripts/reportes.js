e1514b3f9aa0f231025896a9e04fab473bad9857
Cristian Vallejos <cristian.vallejos.14@sansano.usm.cl>
2017-11-22 11:39:46 -0300

    Cambios previos a Reporte General

--- app/assets/javascripts/reportes.js
+++ app/assets/javascripts/reportes.js
@@ -1,9 +1,17 @@
 function showReportePorObra() {
     document.getElementById("reporte_por_obra").style.display = 'block';
+    $("#reportePorObra h3").css("color", 'black');
+    $("#reportePorObra h3").addClass("sub-menu-reportes");
+    $("#reporteGeneral h3").removeClass("sub-menu-reportes");
     document.getElementById("reporte_general").style.display = 'none';
+    $("#reporteGeneral h3").css("color", '#6E6C64');
 }
 
 function showReporteGeneral() {
     document.getElementById("reporte_por_obra").style.display = 'none';
+    $("#reporteGeneral h3").css("color", 'black');
+    $("#reporteGeneral h3").addClass("sub-menu-reportes");
+    $("#reportePorObra h3").removeClass("sub-menu-reportes");
     document.getElementById("reporte_general").style.display = 'block';
+    $("#reportePorObra h3").css("color", '#6E6C64');
 }
